0684e4050bbb6f4a070df79dd9cd47c2c02c3420
“julian <badjab326@gmail.com>
2022-05-17 16:44:40 -0700

    set up database and added drinks route

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const drinks = require('./models/drinks');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the GitPub app!')
+})
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+
+
+app.listen(port, () => {
+    console.log("listening on port", port);
+});
